3ba8d5210d03d57479c3e11030a5c18a2367b1a6
Har <Har@MacBook-Pro.monlib.vic.gov.au>
2017-10-24 15:22:55 +1100

    svm

--- svm_intro.py
+++ svm_intro.py
@@ -28,9 +28,3 @@ accuracy = clf.score(X_test, y_test)
 
 print('Accuracy:', accuracy)
 
-#example_measures = np.array([ [4,2,1,1,1,2,3,2,1], [4,2,1,1,2,3,2,1] ])
-#
-#example_measures = example_measures.reshape(len(example_measures),-1)
-#
-#prediction = clf.predict(example_measures)
-#print(prediction)
